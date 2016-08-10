daab57866a9eb95caa9ee696b9c2d8eee2c4e37e
Harley Trung <harley@bsa.edu.vn>
2016-08-10 18:19:30 +0700

    milestone 2

--- lib/movies.rb
+++ lib/movies.rb
@@ -1,23 +1,9 @@
 require "movies/version"
-require "mechanize"
 
 module Movies
-  # Your code goes here...
   class Search
-    def film_links
-      page.links_with(class: 'film-name')
-    end
-
     def films
-      film_links.map(&:text)
-    end
-
-    def agent
-      @agent ||= Mechanize.new
-    end
-
-    def page
-      @page ||= agent.get("http://www.123phim.vn/phim/")
+      []
     end
   end
 end
--- spec/movies_spec.rb
+++ spec/movies_spec.rb
@@ -5,13 +5,11 @@ describe Movies do
     expect(Movies::VERSION).not_to be nil
   end
 
-  describe "#films" do
+  describe "Search" do
     let(:search) { Movies::Search.new }
 
-    it "should list movies" do
-      WebMock.disable_net_connect!
-      VCR.use_cassette 'search/movies' do
-        puts search.films.inspect
+    describe "#films" do
+      it "should list movies" do
         expect(search.films).to eq [
           "Biệt Đội Cảm Tử - Suicide Squad (NC16)",
           "Vùng Nước Tử Thần - The Shallows (NC16)",
@@ -30,8 +28,7 @@ describe Movies do
           "Kỷ Băng Hà: Trời Sập - Ice Age: Collision Course",
           "Đẳng Cấp Thú Cưng - The Secret Life of Pets",
           "Angry Birds",
-          "Bệnh Viện Ma (NC16)",
-          "Mỹ Nhân Ngư - The Mermaid"]
+          "Bệnh Viện Ma (NC16)", "Mỹ Nhân Ngư - The Mermaid"]
       end
     end
   end
