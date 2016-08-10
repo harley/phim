9c02e6acb8a7f5e862e4125f2824720e513b3143
Harley Trung <harley@bsa.edu.vn>
2016-08-10 17:43:44 +0700

    implement Movies::Search#films with tests

--- movies.gemspec
+++ movies.gemspec
@@ -9,9 +9,9 @@ Gem::Specification.new do |spec|
   spec.authors       = ["Harley Trung"]
   spec.email         = ["harley@harley.io"]
 
-  spec.summary       = %q{TODO: Write a short summary, because Rubygems requires one.}
-  spec.description   = %q{TODO: Write a longer description or delete this line.}
-  spec.homepage      = "TODO: Put your gem's website or public repo URL here."
+  spec.summary       = %q{Write a short summary, because Rubygems requires one.}
+  spec.description   = %q{Write a longer description or delete this line.}
+  spec.homepage      = "http://123phim.vn"
   spec.license       = "MIT"
 
   # Prevent pushing this gem to RubyGems.org. To allow pushes either set the 'allowed_push_host'
@@ -30,4 +30,8 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 1.12"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rspec", "~> 3.0"
+  spec.add_development_dependency "guard-rspec"
+  spec.add_development_dependency "vcr"
+  spec.add_development_dependency "mechanize"
+  spec.add_development_dependency "webmock"
 end
--- spec/movies_spec.rb
+++ spec/movies_spec.rb
@@ -5,7 +5,34 @@ describe Movies do
     expect(Movies::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe "#films" do
+    let(:search) { Movies::Search.new }
+
+    it "should list movies" do
+      WebMock.disable_net_connect!
+      VCR.use_cassette 'search/movies' do
+        puts search.films.inspect
+        expect(search.films).to eq [
+          "Biệt Đội Cảm Tử - Suicide Squad (NC16)",
+          "Vùng Nước Tử Thần - The Shallows (NC16)",
+          "Siêu Điệp Viên Tái Xuất - Jason Bourne (NC16)",
+          "Thám Tử Lừng Danh Conan: Cơn Ác Mộng Đen Tối",
+          "Hồn Ma Trở Lại - The Remains (NC16)",
+          "The BFG - Chuyện Chưa Kể Ở Xứ Sở Khổng Lồ",
+          "Ác Mộng Bóng Đêm - Lights Out (NC16)",
+          "Big Bang MADE: The Movie",
+          "Mầm Chết - Viral (NC16)",
+          "Biệt Đội Bắt Ma 2 - Ghostbusters 2 (NC16)",
+          "Tik Tak, Anh Yêu Em",
+          "Star Trek: Không Giới Hạn - Star Trek Beyond",
+          "Tẩu Thoát Ngoạn Ngục - Skiptrace",
+          "Fan Cuồng",
+          "Kỷ Băng Hà: Trời Sập - Ice Age: Collision Course",
+          "Đẳng Cấp Thú Cưng - The Secret Life of Pets",
+          "Angry Birds",
+          "Bệnh Viện Ma (NC16)",
+          "Mỹ Nhân Ngư - The Mermaid"]
+      end
+    end
   end
 end
